cc985283b7307f16fd69fb5410b06018f521a252
Nada ElFadili <EFNada98@gmail.com>
2021-04-20 23:07:53 +0000

    initiale commit

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+//Db mongoDB
+mongoose.connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('db connected'))
+    .catch(() => console.log('not nonnect to the database !'))
+ 
